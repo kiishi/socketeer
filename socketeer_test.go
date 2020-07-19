77d7cf069f8554b628bf099e4484391ad5847737
kiishi <deolu.asenuga@gmail.com>
2020-07-19 19:18:06 +0100

    fixed test

--- socketeer_test.go
+++ socketeer_test.go
@@ -14,91 +14,74 @@ import (
 	"time"
 )
 
-
 var samplebuffer = new(bytes.Buffer)
 
-// Id generation
-type sample struct {}
+type sampleDispatcher struct{}
 
-func (s *sample) GetUniqueId() string{
-	return "user" + strconv.Itoa(int(time.Now().UnixNano()))
-}
-
-func (s *sample) Run(socketerr *SocketeerManager){
+func (s *sampleDispatcher) Run(socketerr *Manager) {
 	tick := time.NewTicker(5 * time.Second)
-	for{
+	for {
 		<-tick.C
 		socketerr.Broadcast([]byte("hello"))
 	}
 }
 
-func (s *sample) OnMessage(message []byte , sendChannels map[string]chan []byte){
-
-}
-
-
-
-func BootstrapRouter() *http.ServeMux{
-
-
-	manager := SocketeerManager{
-		IdGen: &sample{},
+func BootstrapRouter() *http.ServeMux {
+	manager := Manager{
+		IdGen: func() string {
+			return "user" + strconv.Itoa(int(time.Now().UnixNano()))
+		},
+		OnConnect: func(manager *Manager, request *http.Request, connectionId string) {
+			fmt.Fprint(samplebuffer, "connected")
+		},
 	}
 
-	manager.AddDispatcher(&sample{})
-	manager.AddMessageHandler(&sample{})
-	manager.AddOnConnectHook(func(manager *SocketeerManager , connectionId string) {
-		fmt.Fprint(samplebuffer , "connected")
-	})
+	manager.AddDispatcher(&sampleDispatcher{})
+
 	manager.Init()
 
-	mux:= http.NewServeMux()
-	mux.HandleFunc("/" , func(writer http.ResponseWriter, request *http.Request) {
-		id , _ := manager.Manage(writer, request)
-		manager.SendToId(id , []byte(id))
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		id, _ := manager.Manage(writer, request)
+		manager.SendToId(id, []byte(id))
 	})
 
 	return mux
 }
 
+func ConnectToTestServer(t *testing.T , server *httptest.Server) *websocket.Conn {
 
+	wsURL := "ws" + strings.TrimPrefix(server.URL, "http") + "/"
+	conn, _, err := websocket.DefaultDialer.Dial(wsURL, nil)
+	if err != nil {
+		t.Fatalf("Couldnt connect to websocket : %s", err.Error())
+	}
+	return conn
+}
 
+func TestSocketeerManager_main(t *testing.T) {
+	server := httptest.NewServer(BootstrapRouter())
 
+	t.Run("Recieves connectionId ", func(t *testing.T) {
+		conn := ConnectToTestServer(t, server)
 
-
-
-
-
-func TestSocketeerManager_main(t *testing.T){
-
-	t.Run("Recieve connectionId " , func(t *testing.T) {
-		server := httptest.NewServer(BootstrapRouter())
-
-		wsURL := "ws" + strings.TrimPrefix(server.URL, "http") + "/"
-		conn ,_ , err := websocket.DefaultDialer.Dial(wsURL , nil)
-
-		defer conn.Close()
-
-		if err != nil{
-			t.Fatalf("Couldnt connect to websocket : %s" , err.Error())
-		}
-
-		_ , message , err := conn.ReadMessage()
-
-		if err != nil{
+		_, message, err := conn.ReadMessage()
+		if err != nil {
 			t.Fatal("Couldnt read message from the websocket")
 		}
 
-		assert.Greater(t , len(message) , 0)
+		assert.Greater(t, len(message), 0)
 
-		time.Sleep(11 * time.Second)
-		_ , helloMessage , err := conn.ReadMessage()
+		//Recieves Dispatcher Message
 
-		assert.Equal(t , []byte("hello") , helloMessage)
+		time.Sleep(11 * time.Second) // Bad Practice
+		_, helloMessage, err := conn.ReadMessage()
 
-		value , _ := ioutil.ReadAll(samplebuffer)
+		assert.Equal(t, []byte("hello"), helloMessage)
 
-		assert.Equal(t ,[]byte("connected") , value)
-
+		// OnConnectMessage Ran
+		value, _ := ioutil.ReadAll(samplebuffer)
+		assert.Equal(t, []byte("connected"), value)
 	})
+
 }
